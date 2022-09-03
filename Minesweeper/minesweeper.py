fb5f251b6e731ca38669100e56bef31d7dd616ba
JinKiat0313 <64313197+JinKiat0313@users.noreply.github.com>
2022-09-03 15:59:28 +0800

    Update minesweeper.py

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -126,7 +126,7 @@ def play(dim_size=10, num_bombs=10):
     # Step 1: create the board and plant the bombs
     board = Board(dim_size, num_bombs)
     # Step 2: show the user the board and ask for where they want to dig
-    # Step 3a: if location is a bomb, show game over message
+    # Step 3a: if location is a bomb, show game over 
     # Step 3b: if location is not a bomb, dig recursively until each square is at least next to bomb
     # Step 4: repeat steps 2 and 3a/b until there are no more places to dig -> Victory!
     while len(board.dug) < board.dim_size ** 2 - num_bombs:
