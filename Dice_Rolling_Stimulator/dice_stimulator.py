2537b66e917adaf437dd8e60d431f8791aff7b6a
brunombs <99062708+brunombs@users.noreply.github.com>
2022-09-20 23:51:38 -0300

    Melhorias
    Fiz algumas melhorias, coloquei para o programa apresentar o número que resultou do dado por escrito a cada vez que for rolado, a quantidade de vezes que o dado foi rolado e a soma dos valores que resultou em todas as vezes que o dado foi rolado, além de traduzir o programa para português.

--- Dice_Rolling_Stimulator/dice_stimulator.py
+++ Dice_Rolling_Stimulator/dice_stimulator.py
@@ -1,50 +1,66 @@
 import random
-
-print("This is a dice stimulator")
-x = "y"
-while x == "y":
+print("Esse é um simulador de dados!")
+rolarnovamente = "Y"
+vezesdadorolado = 0
+soma = 0
+while rolarnovamente == "Y":
+    vezesdadorolado += 1
     number = random.randint(1,6)
-
+    soma += number
     if number == 1:
-        print("===========")
+        print('='*11)
         print("|         |")
         print("|    O    |")
         print("|         |")
-        print("===========")
+        print('='*11)
+        print('O dado resultou no número 1')
 
     if number == 2:
-        print("===========")
+        print('='*11)
         print("|         |")
         print("| O     O |")
         print("|         |")
-        print("===========")
+        print('='*11)
+        print('O dado resultou no número 2')
 
     if number == 3:
-        print("===========")
+        print('='*11)
         print("|    O    |")
         print("|    O    |")
         print("|    O    |")
-        print("===========")
+        print('='*11)
+        print('O dado resultou no número 3')
         
     if number == 4:
-        print("===========")
+        print('='*11)
         print("| O     O |")
         print("|         |")
         print("| O     O |")
-        print("===========")
+        print('='*11)
+        print('O dado resultou no número 4')
         
     if number == 5:
-        print("===========")
+        print('='*11)
         print("| O     O |")
         print("|    O    |")
         print("| O     O |")
-        print("===========")
+        print('='*11)
+        print('O dado resultou no número 5')
         
     if number == 6:
-        print("===========")
+        print('='*11)
         print("| O     O |")
         print("| O     O |")
         print("| O     O |")
-        print("===========")
-    
-    x =  input("Press y to roll again ")
+        print('='*11)
+        print('O dado resultou no número 6')
+
+    rolarnovamente =  input("Pressione Y para rolar o dado novamente: ").upper()
+print()
+print('='*10)
+print('Você não pressionou Y, então o simulador de dados foi encerrado.')
+if vezesdadorolado == 1:
+    print(f'O dado foi rolado {vezesdadorolado} vez!')
+else:
+    print(f'O dado foi rolado {vezesdadorolado} vezes!')
+print(f'A soma de todos os valores que o dado simulou foi: {soma}')
