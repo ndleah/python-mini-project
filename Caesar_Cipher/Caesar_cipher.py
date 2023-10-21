0320dffa1f70dab01f3e6aed7fef32aedd040508
jazzpher <80498982+jazzpher@users.noreply.github.com>
2023-10-21 11:55:01 +0800

    Update Caesar_cipher.py
    - Renamed the class to `SubstitutionCipher to follow Python naming conventions.
    - Removed the unnecessary `else: continue` statement in the `encrypt_string` method.
    - Simplified the encryption and decryption logic using list comprehensions and the `get` method of dictionaries to handle missing characters gracefully.
    - Improved variable naming for clarity.

--- Caesar_Cipher/Caesar_cipher.py
+++ Caesar_Cipher/Caesar_cipher.py
@@ -1,45 +1,47 @@
-class main: 
-    def __init__(self,key:dict) -> None:
+class SubstitutionCipher:
+    def __init__(self, key: dict):
         self.key = key
 
-    def get_input(self) -> None: 
+    def get_input(self):
         while True:
-            blank_string = str(input("Enter string to decrypt: "))
+            # Prompt the user for input and convert it to lowercase
+            blank_string = input("Enter a string to decrypt: ").lower()
             if blank_string.isalpha():
-                blank_string = blank_string.lower()
                 self.blank_string = blank_string
                 break
             else:
                 print("Input is not valid")
-                continue
 
-    def encrypt_string(self) -> str:
-        output = ""
-        for c in self.blank_string:
-            for k,v in self.key.items():
-                if k == c:
-                    output += v
-                else:
-                    continue
-        self.decrypted_string = output
-        return(output)
+    def encrypt_string(self):
+        # Use a list comprehension to apply the substitution cipher
+        encrypted_string = ''.join(self.key[c] if c in self.key else c for c in self.blank_string)
+        self.encrypted_string = encrypted_string
+        return encrypted_string
 
-    def decrypt_string(self, string: str) -> str:
-        output = "" 
-        string = string.lower()
-        string = string.strip()
-        if string == "":
-            return(self.blank_string)
-        else: 
-            for c in string:
-                for k,v in self.key.items():
-                    if v == c:
-                        output += k
-        
-        return(output)
+    def decrypt_string(self, string):
+        # Use a list comprehension to reverse the substitution cipher
+        decrypted_string = ''.join(self.key[c] if c in self.key else c for c in string)
+        return decrypted_string
 
 if __name__ == "__main__":
-    key ={"a": "d", "b": "e", "c": "f", "d": "g", "e": "h", "f": "i", "g": "j", "h": "k", "i": "l", "j": "m", "k": "n", "l": "o", "m": "p", "n": "q", "o": "r", "p": "s", "q": "t", "r": "u", "s": "v", "t": "w", "u": "x", "v": "y", "w": "z", "x": "a", "y": "b", "z": "c"}
-    main = main(key=key)
-    main.get_input()
-    print(main.encrypt_string())
+    key = {
+        "a": "d", "b": "e", "c": "f", "d": "g", "e": "h", "f": "i",
+        "g": "j", "h": "k", "i": "l", "j": "m", "k": "n", "l": "o",
+        "m": "p", "n": "q", "o": "r", "p": "s", "q": "t", "r": "u",
+        "s": "v", "t": "w", "u": "x", "v": "y", "w": "z", "x": "a",
+        "y": "b", "z": "c"
+    }
+
+    # Create an instance of the SubstitutionCipher class
+    substitution_cipher = SubstitutionCipher(key)
+
+    # Get user input and store it in the instance
+    substitution_cipher.get_input()
+
+    # Encrypt the input string and print the result
+    encrypted = substitution_cipher.encrypt_string()
+    print(f"Encrypted string: {encrypted}")
+
+    # Decrypt the encrypted string and print the result
+    decrypted = substitution_cipher.decrypt_string(encrypted)
+    print(f"Decrypted string: {decrypted}")
