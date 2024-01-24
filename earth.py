6d37cec713b8791ec8ea1eb6d9ff604aa69fda69
Sohaib Baig <67321108+abdullah818@users.noreply.github.com>
2024-01-24 11:17:54 +0500

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,22 +1,22 @@
 import numpy as np
 from netCDF4 import Dataset
-# download required datset from net
+download required datset from net
 
 def Etopo(lon_area, lat_area, resolution):
-  ### Input
-  # resolution: resolution of topography for both of longitude and latitude [deg]
-  # (Original resolution is 0.0167 deg)
-  # lon_area and lat_area: the region of the map which you want like [100, 130], [20, 25]
-  ###
-  ### Output
-  # Mesh type longitude, latitude, and topography data
-  ###
+  Input
+  resolution: resolution of topography for both of longitude and latitude [deg]
+  (Original resolution is 0.0167 deg)
+  lon_area and lat_area: the region of the map which you want like [100, 130], [20, 25]
   
-  # Read NetCDF data
+  Output
+  Mesh type longitude, latitude, and topography data
+  
+  
+  Read NetCDF data
   data = Dataset("ETOPO1_Ice_g_gdal.grd", "r")
 
   
-  # Get data
+  Get data
   lon_range = data.variables['x_range'][:]
   lat_range = data.variables['y_range'][:]
   topo_range = data.variables['z_range'][:]
@@ -26,20 +26,20 @@ def Etopo(lon_area, lat_area, resolution):
   lon_num = dimension[0]
   lat_num = dimension[1]
   
-  # Prepare array
+  Prepare array
   lon_input = np.zeros(lon_num); lat_input = np.zeros(lat_num)
   for i in range(lon_num):
     lon_input[i] = lon_range[0] + i * spacing[0]
   for i in range(lat_num):
     lat_input[i] = lat_range[0] + i * spacing[1]
 
-  # Create 2D array
+  Create 2D array
   lon, lat = np.meshgrid(lon_input, lat_input)
   
-  # Convert 2D array from 1D array for z value
+  Convert 2D array from 1D array for z value
   topo = np.reshape(z, (lat_num, lon_num))
   
-  # Skip the data for resolution
+  Skip the data for resolution
   if ((resolution < spacing[0]) | (resolution < spacing[1])):
     print('Set the highest resolution')
   else:
@@ -50,13 +50,13 @@ def Etopo(lon_area, lat_area, resolution):
     
   topo = topo[::-1]
   
-  # Select the range of map
+  Select the range of map
   range1 = np.where((lon>=lon_area[0]) & (lon<=lon_area[1]))
   lon = lon[range1]; lat = lat[range1]; topo = topo[range1]
   range2 = np.where((lat>=lat_area[0]) & (lat<=lat_area[1]))
   lon = lon[range2]; lat = lat[range2]; topo = topo[range2]
   
-  # Convert 2D again
+  Convert 2D again
   lon_num = len(np.unique(lon))
   lat_num = len(np.unique(lat))
   lon = np.reshape(lon, (lat_num, lon_num))
@@ -65,11 +65,11 @@ def Etopo(lon_area, lat_area, resolution):
   
   return lon, lat, topo
 def degree2radians(degree):
-  # convert degrees to radians
+  convert degrees to radians
   return degree*np.pi/180
   
 def mapping_map_to_sphere(lon, lat, radius=1):
-  # this function maps the points of coords (lon, lat) to points onto the sphere of radius radius
+  this function maps the points of coords (lon, lat) to points onto the sphere of radius radius
   lon=np.array(lon, dtype=np.float64)
   lat=np.array(lat, dtype=np.float64)
   lon=degree2radians(lon)
@@ -78,12 +78,12 @@ def mapping_map_to_sphere(lon, lat, radius=1):
   ys=radius*np.sin(lon)*np.cos(lat)
   zs=radius*np.sin(lat)
   return xs, ys, zs
-# Import topography data
-# Select the area you want
+Import topography data
+Select the area you want
 resolution = 0.8
 lon_area = [-180., 180.]
 lat_area = [-90., 90.]
-# Get mesh-shape topography data
+Get mesh-shape topography data
 lon_topo, lat_topo, topo = Etopo(lon_area, lat_area, resolution)
 xs, ys, zs = mapping_map_to_sphere(lon_topo, lat_topo)
 Ctopo = [[0, 'rgb(0, 0, 70)'],[0.2, 'rgb(0,90,150)'], 
